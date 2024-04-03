a998c3479e356aecbe2472297a94f8bae44ce903
ManuelBarreiro03 <Manuelbarreiro2003@gmail.com>
2024-04-03 09:43:03 -0300

    123456

--- clsCola.cs
+++ clsCola.cs
@@ -3,11 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
 
 namespace pryPOO
 {
     internal class clsCola
     {
-
+        private clsNodo pri;
+        private clsNodo ult;
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+        public clsNodo Ultimo
+        {
+            get { return ult; }
+            set { ult = value; }
+        }
+        public void Agregar(clsNodo Nuevo) 
+        {
+            if (Primero == null && Ultimo == null)
+            {
+                Primero = Nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = Nuevo;
+            }
+        }
+        public void eliminar() 
+        {
+            if (Primero==Ultimo)
+            {
+                Primero = null;
+                Ultimo = null;
+            }
+            else
+            {
+                Primero = Primero.Siguiente;
+            }
+        }
+        public void Recorrer(DataGridView Grilla) 
+        {
+            clsNodo Aux = Primero;
+            Grilla.Rows.Clear();
+            while (Aux !=null)
+            {
+                Grilla.Rows.Add(Aux.Codigo, Aux.Nombre, Aux.Tramite);
+                Aux = Aux.Siguiente;
+            }
+        }
+        public void Recorrer(ListBox Lista)
+        {
+            clsNodo Aux = Primero;
+            Lista.Items.Clear();
+            while (Aux != null)
+            {
+                Lista.Items.Add(Aux.Codigo);
+                Aux = Aux.Siguiente;
+            }
+        }
+        public void Recorrer(ComboBox Combo) 
+        {
+            clsNodo Aux = Primero;
+            Combo.Items.Clear();
+            while (Aux != null)
+            {
+                Combo.Items.Add(Aux.Codigo);
+                Aux = Aux.Siguiente;
+            }
+        }
     }
 }
